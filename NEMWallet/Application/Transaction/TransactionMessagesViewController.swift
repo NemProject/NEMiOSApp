d27c07f431c8168f145adbc953f6b44b51a83e27
insanely9 <insanaly9@gmail.com>
2017-10-09 18:25:36 +0900

    Change copy button to image

--- NEMWallet/Application/Transaction/TransactionMessagesViewController.swift
+++ NEMWallet/Application/Transaction/TransactionMessagesViewController.swift
@@ -159,8 +159,7 @@ class TransactionMessagesViewController: UIViewController, UIAlertViewDelegate {
     
     /// Creates and adds the compose bar button item to the view controller.
     fileprivate func createBarButtonItem() {
-        
-        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Copy", style: UIBarButtonItemStyle.plain, target: self, action: #selector(copyCorrespondentAddress(_:)))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(named:"Copy"), style: UIBarButtonItemStyle.plain, target: self, action: #selector(copyCorrespondentAddress(_:)))
     }
     
     /**
